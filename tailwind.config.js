837e3d5ee86c72061e03b3bb64b32258e67ce2b6
ChriCass <chris_ccc68@yahoo.com.pe>
2024-10-08 22:52:08 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-', // Prefijo para evitar conflictos
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
